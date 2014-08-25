5e17f4d646e0acef44356928c74407b7231e76c5
kantha <kanthakumar2010@gmail.com>
2014-08-25 20:37:40 +0530

    Add meter code

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -45,10 +45,21 @@ var app = {
      //                }
 // watchPosition
   // function polePosition(){
+
+    var startPos;
         navigator.geolocation.watchPosition(function(position) { 
+           startPos = position;
           document.getElementById("currentLat").innerHTML = position.coords.latitude;
           document.getElementById("currentLon").innerHTML = position.coords.longitude;
           document.getElementById("currentAcu").innerHTML = position.coords.accuracy;
+
+          document.getElementById("distance").innerHTML =
+              calculateDistance(startPos.coords.latitude, startPos.coords.longitude,
+                                position.coords.latitude, position.coords.longitude);
+
+          document.getElementById("distancem").innerHTML =
+              calculateDistance(startPos.coords.latitude, startPos.coords.longitude,
+                                position.coords.latitude, position.coords.longitude) * 1000;
         },
         function(error) {
                 alert('code: '    + error.code    + '\n' +
@@ -57,7 +68,20 @@ var app = {
         { frequency: 3000, enableHighAccuracy: true });
   //}
 
-    // {frequency: 3000, frequencytimeout: 15000, enableHighAccuracy: true}
+    function calculateDistance(lat1, lon1, lat2, lon2) {
+        var R = 6371; // km
+        var dLat = (lat2-lat1).toRad();
+        var dLon = (lon2-lon1).toRad();
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+                Math.sin(dLon/2) * Math.sin(dLon/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c;
+        return d;
+      }
+      Number.prototype.toRad = function() {
+        return this * Math.PI / 180;
+      }
 }
      
 };
